18ff3b74f9906d4db885c389adb93e665c00c3d3
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2023-09-10 19:56:17 +0200

    All pawn masks

--- src/BitBoard.cpp
+++ src/BitBoard.cpp
@@ -9,8 +9,13 @@ unsigned long long BitBoard::ULL_MASK = 0b00000000000000000000000000000000000000
 unsigned long long BitBoard::northMoves[ 64 ];
 unsigned long long BitBoard::southMoves[ 64 ];
 
+unsigned long long BitBoard::northEastMoves[ 64 ];
+unsigned long long BitBoard::southWestMoves[ 64 ];
+
 unsigned long long BitBoard::pawnMovesNormalWhite[ 64 ];
 unsigned long long BitBoard::pawnMovesNormalBlack[ 64 ];
+unsigned long long BitBoard::pawnMovesAttackWhite[ 64 ];
+unsigned long long BitBoard::pawnMovesAttackBlack[ 64 ];
 
 void BitBoard::initialize()
 {
@@ -23,26 +28,62 @@ void BitBoard::initialize()
         northMoves[ square ] = createNorthMask( square );
         southMoves[ square ] = createSouthMask( square );
 
+        //northEastMoves[ square ] = createNorthEastMask( square );
+        //southWestMoves[ square ] = createSouthWestMask( square );
+
         // Pawns
         pawnMovesNormalWhite[ square ] = 0;
         pawnMovesNormalBlack[ square ] = 0;
+        pawnMovesAttackWhite[ square ] = 0;
+        pawnMovesAttackBlack[ square ] = 0;
 
+        // Pawns don't move from first or last ranks
         if ( rank < 7 && rank > 0 )
         {
+            // White pawn
             pawnMovesNormalWhite[ square ] = ULL_MASK << ( square + 8 );
 
+            // Initial double move
             if ( rank == 1 )
             {
                 pawnMovesNormalWhite[ square ] |= ULL_MASK << ( square + 16 );
             }
-        }
-        if ( rank > 0 && rank < 7 )
-        {
+
+            // Capture
+            if ( file == 7 )
+            {
+                pawnMovesAttackWhite[ square ] = ULL_MASK << ( square + 7 );
+            }
+            else if ( file == 0 )
+            {
+                pawnMovesAttackWhite[ square ] = ULL_MASK << ( square + 9 );
+            }
+            else
+            {
+                pawnMovesAttackWhite[ square ] = (ULL_MASK << ( square + 7 )) | (ULL_MASK << ( square + 9 ));
+            }
+
+            // Black pawn
             pawnMovesNormalBlack[ square ] = ULL_MASK << ( square - 8 );
 
+            // Initial double move
             if ( rank == 6 )
             {
                 pawnMovesNormalBlack[ square ] |= ULL_MASK << ( square - 16 );
+            }
+
+            // Capture
+            if ( file == 7 )
+            {
+                pawnMovesAttackBlack[ square ] = ULL_MASK << ( square - 9 );
+            }
+            else if ( file == 0 )
+            {
+                pawnMovesAttackBlack[ square ] = ULL_MASK << ( square - 7 );
+            }
+            else
+            {
+                pawnMovesAttackBlack[ square ] = ( ULL_MASK << ( square - 7 ) ) | ( ULL_MASK << ( square - 9 ) );
             }
         }
 
@@ -51,7 +92,10 @@ void BitBoard::initialize()
         //dumpBitBoard( southMoves[ square ], " South" );
         //dumpBitBoard( northMoves[square] | southMoves[ square ], " Combined" );
         //dumpBitBoard( pawnMovesNormalWhite[ square ], " White Pawn" );
+        dumpBitBoard( pawnMovesAttackWhite[ square ], " White Pawn Attack" );
         //dumpBitBoard( pawnMovesNormalBlack[ square ], " Black Pawn" );
+        dumpBitBoard( pawnMovesAttackBlack[ square ], " Black Pawn Attack" );
+        //dumpBitBoard( northEastMoves[square] | southWestMoves[ square ], " Combined" );
     }
 }
 
